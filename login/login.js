327fe920683b512c07f31b98b30ae8ee73c47dce
Julia Fracola <134508829+baehyunchu@users.noreply.github.com>
2023-09-20 07:56:03 -0300

    Add files via upload

---
+++ login/login.js
@@ -0,0 +1,15 @@
+lgn = "julia";
+pwd = "12356";
+
+function autenticar(){
+    var log = document.getElementById('login').value;
+    var pass = document.getElementById('passwd').value;
+
+    if(log == lgn & pass == pwd){
+        alert('fon')
+    }else{
+        alert('firifonfon')
+    }
+
+    }
+    
